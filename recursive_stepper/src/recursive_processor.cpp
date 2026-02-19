c4ef332712f3dec4367a1f86b3126451d47b2613
walrus <walrus@local>
2026-02-19 18:09:02 +0700

    Recursive stepper: include hidden/system entries

--- recursive_stepper/src/recursive_processor.cpp
+++ recursive_stepper/src/recursive_processor.cpp
@@ -13,7 +13,7 @@ QStringList RecursiveProcessor::collectFilePaths(
     return files;
   }
 
-  QDirIterator iterator(root.absolutePath(), QDir::Files | QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
+  QDirIterator iterator(root.absolutePath(), QDir::Files | QDir::NoDotAndDotDot | QDir::Hidden | QDir::System, QDirIterator::Subdirectories);
   while (iterator.hasNext()) {
     const QString path = iterator.next();
     const QFileInfo info(path);
